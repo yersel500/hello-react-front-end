1d3c239a7848edda356b041f0845d86402f77413
yersel500 <yersel500@gmail.com>
2022-07-05 21:33:32 -0500

    configure Store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import greeting from './greeting';
+
+const reducer = combineReducers({
+  greeting
+});
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+export default store;
